94a424c53d63fa25ccfa338b95ae48016430682a
SanyaSho <68691958+sanyasho@users.noreply.github.com>
2024-06-20 18:27:15 +0300

    Initial commit

---
+++ rungame.bat
@@ -0,0 +1,2 @@
+@echo off
+start portal2.exe -game portal2 -insecure -sw -noborder
---
+++ rungameds.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Change the -ip and -port param to match yours, or remove them entirely if you don't need to.
+start srcds.exe -game portal2 -console -insecure -ip 192.168.1.37 -port 27016
